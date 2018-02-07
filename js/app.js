9369db157ea8aba1d67cfa145c78073362cb52af
Hanna <hajung96@gmail.com>
2018-02-07 14:28:38 +0100

    started view

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ $(function() {
 	
 	// And create the instance of ExampleView
 	var exampleView = new ExampleView($("#exampleView"));
+	var recipe = new Recipe($("#recipe"),model);
 
 	/**
 	 * IMPORTANT: app.js is the only place where you are allowed to
