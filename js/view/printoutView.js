3f465c6f8daa41fbd6f8b5d3d3195fe473db460d
Hanna <hajung96@gmail.com>
2018-02-14 11:56:56 +0100

    printoutView

--- js/view/printoutView.js
+++ js/view/printoutView.js
@@ -33,12 +33,12 @@ var Printout = function (container, model) {
 			newDiv1.className = "row";
 
 			var newDiv2 = document.createElement("div");
-			newDiv2.className = "col-2";
+			newDiv2.className = "col-3";
 			newDiv2.style = "text-align: left;";
 			newDiv1.appendChild(newDiv2);
 
 			var newImg1 = document.createElement("img");
-			newImg1.style = "width: 80%;";
+			newImg1.style = "width: 90%;";
 			newImg1.src = "images/"+dish.image;
 			newDiv2.appendChild(newImg1);
 
@@ -58,19 +58,19 @@ var Printout = function (container, model) {
 			newDiv3.appendChild(newp1);
 
 			var newDiv4 = document.createElement("div");
-			newDiv4.className = "col-4";
+			newDiv4.className = "col-5";
 			newDiv4.style = "text-align: left;";
 			newDiv1.appendChild(newDiv4);
 
-			var newh5 = document.createElement("h2");
+			var newh5 = document.createElement("h4");
 			var newContent = document.createTextNode("Preparation");
 			newh5.appendChild(newContent);
-			newDiv3.appendChild(newh5);
+			newDiv4.appendChild(newh5);
 
 			var newp2 = document.createElement("p");
 			var newContent = document.createTextNode(dish.description);
 			newp2.appendChild(newContent);
-			newDiv3.appendChild(newp2);
+			newDiv4.appendChild(newp2);
 
 			
 			printout.push(newDiv1);
